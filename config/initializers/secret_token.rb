eb73715b70ac4e39aa962d616d95b02c900023a1
katevda <katevda@gmail.com>
2013-07-07 14:44:22 +0200

    First commit.
    Created rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Superstars::Application.config.secret_key_base = '1b201f0a9c069e859a1134d067494579c24e75c93f39a599833a771eecf58749cbadf9448eff843bf11f03c48a1f90c00beafbe1ae0991bb71fd29ca476b48f9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Superstars::Application.config.session_store :cookie_store, key: '_superstars_session'
